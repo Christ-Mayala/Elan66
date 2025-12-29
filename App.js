3a172d11d5b1d25704c339437cdde4add41b531b
Capy <capy@capy.ai>
2025-12-29 19:00:34 +0000

    fix: avoid Expo Router by renaming src/app
    Co-authored-by: Christ-Mayala <servergold2012@gmail.com>

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { AppProviders } from './src/app/providers/AppProviders';
+import { AppProviders } from './src/appShell/providers/AppProviders';
 import { configureNotifications, requestNotifPermissions } from './src/core/services/notifications';
 import { installDevBooleanPropsGuard } from './src/core/utils/devBooleanPropsGuard';
 
